f7c3f7c8b3db66a95e150b75c19d13a1d59bd5f7
lonestardemocrat <lonestardemocrats@gmail.com>
2025-12-12 16:18:17 -0600

    Update 20251212153500_create_texdem_events_index.rb

--- db/migrate/20251212153500_create_texdem_events_index.rb
+++ db/migrate/20251212153500_create_texdem_events_index.rb
@@ -1,38 +1,35 @@
 # frozen_string_literal: true
 
-class CreateTexdemEventsIndex < ActiveRecord::Migration[7.0]
+class CreateTexdemEventsEventIndex < ActiveRecord::Migration[7.0]
   def change
-    create_table :texdem_events_index do |t|
-      t.integer :post_id, null: false
-      t.integer :topic_id
-      t.integer :category_id
+    create_table :texdem_events_event_index do |t|
+      t.integer  :post_id, null: false
+      t.integer  :topic_id, null: false
+      t.integer  :category_id
+      t.string   :visibility, null: false, default: "internal"
 
-      t.string  :title
+      t.string   :title
       t.datetime :starts_at
       t.datetime :ends_at
-      t.string  :timezone
+      t.string   :timezone
 
-      t.string  :location_name
-      t.string  :address
-      t.string  :city
-      t.string  :state
-      t.string  :zip
-      t.string  :visibility
+      t.string   :location_name
+      t.string   :address
+      t.string   :city
+      t.string   :state
+      t.string   :zip
+      t.float    :lat
+      t.float    :lng
 
-      t.float :lat
-      t.float :lng
+      t.string   :external_url
+      t.string   :graphic_url
 
-      t.string :external_url
-      t.string :graphic_url
-      t.string :source  # e.g. "ics", "modal", etc.
-
-      t.timestamps
+      t.datetime :indexed_at, null: false
     end
 
-    add_index :texdem_events_index, :post_id, unique: true
-    add_index :texdem_events_index, :starts_at
-    add_index :texdem_events_index, :topic_id
-    add_index :texdem_events_index, :category_id
-    add_index :texdem_events_index, [:lat, :lng]
+    add_index :texdem_events_event_index, :post_id, unique: true
+    add_index :texdem_events_event_index, [:visibility, :starts_at]
+    add_index :texdem_events_event_index, :topic_id
+    add_index :texdem_events_event_index, :category_id
   end
 end
